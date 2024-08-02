73f4dc8d98c6c851eb17ed2a8e3fbbd2e52a7154
HUNBO SHIM <19010247shim@gmail.com>
2024-08-02 16:31:23 +0900

    왼쪽 만들기

--- voicebot_stt_tts.py
+++ voicebot_stt_tts.py
@@ -4,8 +4,22 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+from audiorecorder import audiorecorder
+from datetime import datetime
 api_key = os.environ.get('OPEN_API_KEY')
 client = openai.OpenAI(api_key=api_key)
+
+def STT(speech):
+    filename = 'input.mp3'
+    speech.export(filename,format='mp3')
+
+    with open(filename,"rb") as audio_file:
+        transcription = client.audio.transcriptions.create(
+            model ="whisper-1",
+            file = audio_file
+        )
+    os.remove(filename)
+    return transcription.text    
 
 
 def main():
@@ -14,6 +28,7 @@ def main():
     st.header("음성 챗봇 프로그램")
 
     st.markdown("---")
+
     with st.expander("음성 챗봇 프로그램에 관하여", expanded = True):
         st.write(
             """
@@ -48,10 +63,22 @@ def main():
     with col1:
         st.subheader("질문하기")
 
+        audio = audiorecorder()
+        if (audio.duration_seconds > 0) and (st.session_state["check_reset"]==False):
+            st.audio(audio.export().read())
+
+            question = STT(audio)
+
+            now = datetime.now().strftime("$H:%M")
+            st.session_state["chat"] = st.session_state["chat"] + [("user", now, question)]
+            st.session_state["messages"] = st.session_state["messages"] + [{"role": "user", "content": question}]
+
+
+
     with col2:
         st.subheader("질문/답변")
 
 
+
 if __name__ == "__main__":
-    print(__name__, "실행")
     main()
